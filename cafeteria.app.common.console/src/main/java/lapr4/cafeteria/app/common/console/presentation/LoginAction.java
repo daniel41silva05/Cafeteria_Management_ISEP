77ef70c12429dcb63d966015ba52dbf84b13ef54
Daniel André Freitas Duarte Silva <1231046@isep.ipp.pt>
2025-09-18 03:08:44 +0100

    US001 - Implement login functionality (#1)

---
+++ cafeteria.app.common.console/src/main/java/lapr4/cafeteria/app/common/console/presentation/LoginAction.java
@@ -0,0 +1,28 @@
+package lapr4.cafeteria.app.common.console.presentation;
+
+import eapli.framework.actions.Action;
+import eapli.framework.infrastructure.authz.domain.model.Role;
+import lapr4.cafeteria.infrastructure.AuthenticationCredentialHandler;
+
+public class LoginAction implements Action {
+
+	private final Role onlyWithThis;
+
+	public LoginAction() {
+		onlyWithThis = null;
+	}
+
+	/**
+	 *
+	 * @param onlyWithThis only if the user has this specific action right will be
+	 *                     allowed to login
+	 */
+	public LoginAction(final Role onlyWithThis) {
+		this.onlyWithThis = onlyWithThis;
+	}
+
+	@Override
+	public boolean execute() {
+		return new LoginUI(new AuthenticationCredentialHandler(), onlyWithThis).show();
+	}
+}
