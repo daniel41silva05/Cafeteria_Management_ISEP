7b50c1cb65ab526797164fdb3b20be5c98aab94c
Daniel André Freitas Duarte Silva <1231046@isep.ipp.pt>
2025-09-20 23:59:20 +0100

    US008 - Implement register dish use case (#8)

---
+++ cafeteria.core/src/main/java/lapr4/cafeteria/dish_management/application/RegisterDishController.java
@@ -0,0 +1,33 @@
+package lapr4.cafeteria.dish_management.application;
+
+import eapli.framework.application.UseCaseController;
+import eapli.framework.general.domain.model.Description;
+import eapli.framework.general.domain.model.Designation;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.money.domain.model.Money;
+import eapli.framework.validations.Preconditions;
+import lapr4.cafeteria.dish_management.domain.Dish;
+import lapr4.cafeteria.dish_management.domain.DishType;
+import lapr4.cafeteria.dish_management.domain.NutricionalInfo;
+import lapr4.cafeteria.dish_management.repositories.DishRepository;
+import lapr4.cafeteria.infrastructure.persistence.PersistenceContext;
+import lapr4.cafeteria.user_management.domain.CafeteriaRoles;
+
+@UseCaseController
+public class RegisterDishController {
+
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    private final DishRepository repo = PersistenceContext.repositories().dishes();
+
+    public Dish registerDish(final String name, final DishType dishType, final double price, final String shortDescription,
+                                 final String longDescription, final int calories, final int salt) {
+        authz.ensureAuthenticatedUserHasAnyOf(CafeteriaRoles.POWER_USER, CafeteriaRoles.MENU_MANAGER);
+
+        Dish newDish = new Dish(Designation.valueOf(name), dishType, Description.valueOf(shortDescription),
+                Description.valueOf(longDescription), Money.euros(price), NutricionalInfo.valueOf(calories, salt));
+
+        return repo.save(newDish);
+    }
+
+}
