3f4ef784da065b4ae2a6a00d03403dea015864de
Daniel André Freitas Duarte Silva <1231046@isep.ipp.pt>
2025-09-23 23:45:03 +0100

    US010 - Add user interface for update dish price or nutricional info (#10)

---
+++ cafeteria.app.backoffice.console/src/main/java/lapr4/cafeteria/app/backoffice/console/presentation/dishes/ChangeDishUI.java
@@ -0,0 +1,67 @@
+package lapr4.cafeteria.app.backoffice.console.presentation.dishes;
+
+import eapli.framework.domain.repositories.ConcurrencyException;
+import eapli.framework.io.util.Console;
+import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.SelectWidget;
+import lapr4.cafeteria.dish_management.application.ChangeDishController;
+import lapr4.cafeteria.dish_management.domain.Dish;
+
+@SuppressWarnings("squid:S106")
+public class ChangeDishUI extends AbstractUI {
+
+    private final ChangeDishController controller = new ChangeDishController();
+
+    @Override
+    protected boolean doShow() {
+        final Iterable<Dish> dishes = controller.listDish();
+
+        if (!dishes.iterator().hasNext()) {
+            System.out.println("There are no dishes");
+        } else {
+            DishPrinter printer = new DishPrinter();
+            final SelectWidget<Dish> selector = new SelectWidget<>(printer.header(), dishes, printer);
+            selector.show();
+            Dish selectedDish = selector.selectedElement();
+            if (selectedDish != null) {
+                try {
+                    final boolean changePrice = Console.readBoolean("Do you want to change the price? (y/n)");
+                    if (changePrice) {
+                        final double newPrice = Console.readDouble("New Price: ");
+                        selectedDish = controller.changeDishPrice(selectedDish, newPrice);
+                    }
+                    final boolean changeNutricionalInfo = Console.readBoolean("Do you want to change the nutricional info? (y/n)");
+                    if (changeNutricionalInfo) {
+                        final int newCalories = Console.readInteger("New Calories: ");
+                        final int newSalt = Console.readInteger("New Salt: ");
+                        selectedDish = controller.changeDishNutricionalInfo(selectedDish, newCalories, newSalt);
+                    }
+                    showDishResult(selectedDish);
+                } catch (@SuppressWarnings("unused") final ConcurrencyException ex) {
+                    System.out.println(
+                            "WARNING: That entity has already been changed or deleted since you last read it");
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void showDishResult(final Dish dish) {
+        System.out.println("\n=== Registration Successful ===");
+
+        System.out.println("Name: " + dish.identity().toString());
+        System.out.println("State: " + (dish.isActive() ? "Active" : "Inactive"));
+        System.out.println("Dish Type: " + dish.dishType().toString());
+        System.out.println("Price: " + dish.price().toString());
+        System.out.println("Short Description: " + dish.shortDescription().toString());
+        System.out.println("Calories: " + dish.nutricionalInfo().calories().toString());
+        System.out.println("Salt: " + dish.nutricionalInfo().salt().toString());
+    }
+
+    @Override
+    public String headline() {
+        return "Change Dish";
+    }
+
+}
